48b1f1814e19fdcc818b6f1ffff45c09f92af752
cecrack <cecraft@gmail.com>
2024-02-07 20:42:56 -0700

    Carrera con hilos

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace F1_sexo_perez_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.lblganando = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPodio = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTercer_Puesto = new System.Windows.Forms.Label();
+            this.lblPrimer_Puesto = new System.Windows.Forms.Label();
+            this.lblSegundo_Puesto = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 550);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(975, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 549);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Font = new System.Drawing.Font("Arial Narrow", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltexto.Location = new System.Drawing.Point(139, 566);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(247, 62);
+            this.lbltexto.TabIndex = 2;
+            this.lbltexto.Text = "Va ganado:";
+            // 
+            // lblganando
+            // 
+            this.lblganando.AutoSize = true;
+            this.lblganando.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblganando.Location = new System.Drawing.Point(386, 573);
+            this.lblganando.Name = "lblganando";
+            this.lblganando.Size = new System.Drawing.Size(0, 55);
+            this.lblganando.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox2.Location = new System.Drawing.Point(-11, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(968, 21);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox3.Location = new System.Drawing.Point(-11, 382);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(968, 21);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox4.Location = new System.Drawing.Point(-56, 523);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1013, 21);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(206, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(610, 62);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Gran premio de Mexico";
+            // 
+            // lblPodio
+            // 
+            this.lblPodio.AutoSize = true;
+            this.lblPodio.Font = new System.Drawing.Font("Elephant", 26F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPodio.ForeColor = System.Drawing.Color.Green;
+            this.lblPodio.Location = new System.Drawing.Point(1338, 9);
+            this.lblPodio.Name = "lblPodio";
+            this.lblPodio.Size = new System.Drawing.Size(184, 67);
+            this.lblPodio.TabIndex = 8;
+            this.lblPodio.Text = "Podio";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox5.BackgroundImage = global::F1_sexo_perez_.Properties.Resources.A;
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(659, 439);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTercer_Puesto);
+            this.panel1.Controls.Add(this.lblPrimer_Puesto);
+            this.panel1.Controls.Add(this.lblSegundo_Puesto);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Location = new System.Drawing.Point(1206, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(659, 439);
+            this.panel1.TabIndex = 10;
+            // 
+            // lblTercer_Puesto
+            // 
+            this.lblTercer_Puesto.AutoSize = true;
+            this.lblTercer_Puesto.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTercer_Puesto.Location = new System.Drawing.Point(431, 148);
+            this.lblTercer_Puesto.Name = "lblTercer_Puesto";
+            this.lblTercer_Puesto.Size = new System.Drawing.Size(0, 36);
+            this.lblTercer_Puesto.TabIndex = 12;
+            // 
+            // lblPrimer_Puesto
+            // 
+            this.lblPrimer_Puesto.AutoSize = true;
+            this.lblPrimer_Puesto.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimer_Puesto.Location = new System.Drawing.Point(256, 0);
+            this.lblPrimer_Puesto.Name = "lblPrimer_Puesto";
+            this.lblPrimer_Puesto.Size = new System.Drawing.Size(0, 36);
+            this.lblPrimer_Puesto.TabIndex = 11;
+            // 
+            // lblSegundo_Puesto
+            // 
+            this.lblSegundo_Puesto.AutoSize = true;
+            this.lblSegundo_Puesto.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundo_Puesto.Location = new System.Drawing.Point(111, 113);
+            this.lblSegundo_Puesto.Name = "lblSegundo_Puesto";
+            this.lblSegundo_Puesto.Size = new System.Drawing.Size(0, 36);
+            this.lblSegundo_Puesto.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1964, 1148);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblPodio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblganando);
+            this.Controls.Add(this.lbltexto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbltexto;
+        private System.Windows.Forms.Label lblganando;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPodio;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTercer_Puesto;
+        private System.Windows.Forms.Label lblPrimer_Puesto;
+        private System.Windows.Forms.Label lblSegundo_Puesto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F1_sexo_perez_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Graphics redbullRuedas, ferrariRuedas, mercedesRuedas, ferrariCarroseria, redbullCarroseria, mercedesCarroseria;
+        Pen plumaBlue = new Pen(Color.Blue, 2);
+        Pen plumaRed = new Pen(Color.Red, 2);
+        Pen plumaGreen = new Pen(Color.Green, 2);
+        Random random = new Random();
+
+        Thread HiloBlue;
+        Thread HiloRed;
+        Thread HiloGreen;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                if (lblganando.Text != "")
+                {
+
+                    lblPrimer_Puesto.Text = "";
+                    lblSegundo_Puesto.Text = "";
+                    lblTercer_Puesto.Text = "";
+                    VelBull = 0;
+                    VelFerrari = 0;
+                    VelMercedes = 0;
+                    lblganando.Text = "";
+                    HiloRed.Abort();
+                    HiloBlue.Abort();
+                    HiloGreen.Abort();
+                  
+
+
+                }
+                else
+                {
+
+                    HiloRed = new Thread(Ferrari);
+                    HiloBlue = new Thread(Redbull);
+                    HiloGreen = new Thread(Aston_martin);
+                    HiloRed.Start();
+                    HiloBlue.Start();
+                    HiloGreen.Start();
+                button1.Enabled = false;    
+                }
+            
+
+        }
+
+        int VelBull;
+        int VelFerrari;
+        int VelMercedes;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            redbullCarroseria=this.CreateGraphics();
+            ferrariCarroseria = this.CreateGraphics();
+            mercedesCarroseria = this.CreateGraphics(); 
+            redbullRuedas=this.CreateGraphics();    
+            ferrariRuedas = this.CreateGraphics();
+            mercedesRuedas = this.CreateGraphics();
+        }
+
+        void Redbull()
+        {               
+            for (int i = 0;i<600;i++) 
+            {
+               
+                redbullCarroseria.DrawRectangle(plumaBlue, VelBull, 90, 50,25);
+                redbullRuedas.DrawEllipse(plumaBlue, VelBull-10, 110,20,20);
+                redbullRuedas.DrawEllipse(plumaBlue, VelBull +40, 110,20,20);
+
+                VelBull += Convert.ToInt32(random.Next(7));
+
+                if(VelBull>600)
+                {
+                    break;
+                }
+
+                Thread.Sleep(3);
+                redbullCarroseria.Clear(Color.White);
+                if (VelBull > VelFerrari && VelBull > VelMercedes)
+                {
+                    lblganando.Text = "Redbull";    
+                    lblganando.ForeColor = Color.Blue;
+                }
+                else if (VelFerrari > VelBull && VelFerrari > VelMercedes)
+                {
+                    lblganando.Text = "Ferrari";
+                    lblganando.ForeColor = Color.Red;
+                }
+                else if (VelMercedes > VelBull && VelMercedes > VelFerrari)
+                {
+                    lblganando.Text = "Aston martin";
+                    lblganando.ForeColor = Color.Green;
+                }
+            }
+            podio("Redbull");
+        }
+
+        void Ferrari()
+        {
+            for (int i = 0; i < 600; i++)
+            {
+                ferrariCarroseria.DrawRectangle(plumaRed, VelFerrari, 190, 50, 25);
+                ferrariRuedas.DrawEllipse(plumaRed, VelFerrari - 10, 210, 20, 20);
+                ferrariRuedas.DrawEllipse(plumaRed, VelFerrari + 40, 210, 20, 20);
+
+                VelFerrari += Convert.ToInt32(random.Next(7));
+                if (VelFerrari > 600)
+                {
+                    break;
+                }
+
+                Thread.Sleep(3);
+                ferrariCarroseria.Clear(Color.White);
+                if (VelBull > VelFerrari && VelBull > VelMercedes)
+                {
+                    lblganando.Text = "Redbull";
+                    lblganando.ForeColor = Color.Blue;
+                }
+                else if (VelFerrari > VelBull && VelFerrari > VelMercedes)
+                {
+                    lblganando.Text = "Ferrari";
+                    lblganando.ForeColor = Color.Red;
+                }
+                else if (VelMercedes > VelBull && VelMercedes > VelFerrari)
+                {
+                    lblganando.Text = "Aston martin";
+                    lblganando.ForeColor = Color.Green;
+                }
+            }
+            podio("Ferrari");
+        }
+
+
+        void Aston_martin()
+        {
+            for (int i = 0; i < 600; i++)
+            {
+                mercedesCarroseria.DrawRectangle(plumaGreen, VelMercedes, 290, 50, 25);
+            mercedesRuedas.DrawEllipse(plumaGreen, VelMercedes - 10, 310, 20, 20);
+               mercedesRuedas.DrawEllipse(plumaGreen, VelMercedes + 40, 310, 20, 20);
+
+                VelMercedes += Convert.ToInt32(random.Next(7));
+                if (VelMercedes > 600)
+                {
+                    break;
+                }
+
+                Thread.Sleep(3);
+                mercedesCarroseria.Clear(Color.White);
+
+                if (VelBull > VelFerrari && VelBull > VelMercedes)
+                {
+                    lblganando.Text = "Redbull";
+                    lblganando.ForeColor = Color.Blue;
+                }
+                else if (VelFerrari > VelBull && VelFerrari > VelMercedes)
+                {
+                    lblganando.Text = "Ferrari";
+                    lblganando.ForeColor = Color.Red;
+                }
+                else if (VelMercedes > VelBull && VelMercedes > VelFerrari)
+                {
+                    lblganando.Text = "Aston martin";
+                    lblganando.ForeColor = Color.Green;
+                }
+
+            }
+            podio("Aston Martin");
+        }
+
+
+        void podio(string lugar)
+        {
+            if (lblPrimer_Puesto.Text == "")
+            {
+                lblPrimer_Puesto.Text = lugar;
+                if (lugar == "Redbull")
+                {
+                    lblPrimer_Puesto.ForeColor = Color.Blue;
+                }
+                else if (lugar == "Ferrari")
+                { 
+                    lblPrimer_Puesto.ForeColor= Color.Red;
+                }
+                else
+                {
+                    lblPrimer_Puesto.ForeColor=Color.Green; 
+                }
+                button1.Enabled = true;
+            }
+
+           else if (lblSegundo_Puesto.Text == "")
+            {
+                lblSegundo_Puesto.Text = lugar;
+                if (lugar == "Redbull")
+                {
+                    lblSegundo_Puesto.ForeColor = Color.Blue;
+                }
+                else if (lugar == "Ferrari")
+                {
+                    lblSegundo_Puesto.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblSegundo_Puesto.ForeColor = Color.Green;
+                }
+            }
+
+            else
+            {
+                lblTercer_Puesto.Text = lugar;
+                if (lugar == "Redbull")
+                {
+                    lblTercer_Puesto.ForeColor = Color.Blue;
+                }
+                else if (lugar == "Ferrari")
+                {
+                    lblTercer_Puesto.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblTercer_Puesto.ForeColor = Color.Green;
+                }
+            }
+        }
+
+
+        
+    }
+}
